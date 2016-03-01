4018f2ba28afc817c01f3cebdc737a34e78024dc
shinyahirai <908.shinya@gmail.com>
2016-03-02 07:44:22 +0800

    SELECT INSERT入門

--- php_basic/contact2/dbconnect.php
+++ php_basic/contact2/dbconnect.php
@@ -1,9 +1,9 @@
 <?php
     // 変数
-    $dsn = 'mysql:dbname=phpkiso2;host=localhost';
+    $dsn = 'mysql:dbname=phpkiso3;host=localhost';
 
     // 定数
-    define('PDO_DSN', 'mysql:dbname=phpkiso2;host=localhost');
+    define('PDO_DSN', 'mysql:dbname=phpkiso3;host=localhost');
 
     $user = 'root';
     // XAMPPのMySQLにはあらかじめrootユーザーが設定されています。
@@ -30,11 +30,54 @@
     // SQL文を実行
     $stmt->execute();
 
+    // SELECT文で取得したデータの表示ほげほげ
+    var_dump($stmt); // 実際に使えるデータではなく、objectというものが返ってくる
+
+    // 真偽値の種類
+    // bool型 true / false
+    // int型 1 / 0
+
+    while (1) {
+        $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+        // $stmtに格納されたobjectから値をひとつひとつ取り出して$recに入れる
+        // 例
+        // $rec = array("id"=>"1", "nickname"=>"user1", "email"=>"user1@gmail.com", "content"=>"プログラミングしたい！");
+        // $recの中にcontactテーブルのカラム名をキーにして値は入っている
+
+        if ($rec == false) {
+            // 繰り返し文から強制的に抜ける
+            break;
+        }
+
+        // どれだけ変数などの値を出力して確認してみるかがプログラミング学習では大事
+        // echo "<pre>";
+        // var_dump($rec);
+        // echo "</pre>";
+
+        // PHPの連想配列からキーを指定してデータを取得すう方法
+        echo "<br>";
+        echo $rec["id"];
+        echo $rec["nickname"];
+        echo $rec["email"];
+        echo $rec["content"];
+        echo "<br>";
+
+    }
+
     // DB接続を切断
     $dbh = null;
 
 ?>
 
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title></title>
+</head>
+<body>
+
+</body>
+</html>
 
 
 
--- php_basic/contact2/thanks.php
+++ php_basic/contact2/thanks.php
@@ -6,6 +6,48 @@
     $nickname = $_POST['nickname'];
     $email = $_POST['email'];
     $content = $_POST['content'];
+
+    // 定数名は大文字で記載し、単語をアンダースコアで区切る
+    define('PDO_DSN', 'mysql:dbname=phpkiso3;host=localhost');
+    define('DB_USERNAME', 'root');
+    define('DB_PASSWORD', '');
+
+    $dbh = new PDO(PDO_DSN,DB_USERNAME,DB_PASSWORD);
+    $dbh->query('SET NAMES utf8');
+
+    // INSERT文1
+    // INSERT INTO テーブル名 (カラム1, カラム2, カラム3 ...) VALUES (値1, 値2, 値3 ...)
+    // $sql = 'INSERT INTO contact (`id`, `nickname`, `email`, `content`) VALUES (NULL, "user4", "user4@gmail.com", "MySQL学びたい")';
+
+    // 文字連結復習
+    // $nickname= "shinya";
+    // echo 'A$nicknameB'; // 出力結果→  A$nicknameB
+    // echo 'A' . $nickname . 'B'; // 出力結果→  AshinyaB
+    // 結論 : 変数と文字を組み合わせて任意の文字列を作るには文字列演算子を使って文字連結させる
+
+    // INSERT文2
+    // INSERT INTO テーブル名 SET カラム1="値1", カラム2="値2"....
+    $sql = 'INSERT INTO contact SET nickname="'
+            .$nickname.
+            '", email="'
+            .$email.
+            '", content="'
+            .$content.
+            '"';
+    // $nickname = 'user1';
+    // $email = 'user1@gmail.com';
+    // $content = 'プログラミングしたい！';
+    // 上記値がフォームに入力されたと仮定した場合の上記$sqlの内容結果
+    // 'INSERT INTO contact SET nickname="user1", email="user1@gmail.com", content="プログラミングしたい！"'
+    
+    // prepare
+    $stmt = $dbh->prepare($sql);
+
+    // execute
+    $stmt->execute();
+
+    // DB切断
+    $dbh = null;
 ?>
 
 <!DOCTYPE html>
